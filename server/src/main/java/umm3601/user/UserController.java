03a1c29cf3fdf7982455559cdb78fc866d74843e
Nic McPhee <mcphee@morris.umn.edu>
2022-01-31 17:13:31 -0600

    Fix failing test in `UserControllerSpec`
    When I changed things in the previous commit, I deleted the line that set the status to 201 when we successfully found a user. @helloworld12321 had pointed out that this wasn't the correct code, and I somehow thought I didn't need to set an explicit code in this case.
    
    Turns out I *do* need to set an explicit code if we're going to check for it in the tests (which we do).
    
    I also discovered, though, that there's an `HttpCode` class in Javalin that does a nice job of including all the standard HTTP response codes, so I switched to using that.

--- server/src/main/java/umm3601/user/UserController.java
+++ server/src/main/java/umm3601/user/UserController.java
@@ -1,6 +1,7 @@
 package umm3601.user;
 
 import io.javalin.http.Context;
+import io.javalin.http.HttpCode;
 import io.javalin.http.NotFoundResponse;
 
 /**
@@ -33,6 +34,7 @@ public class UserController {
     User user = database.getUser(id);
     if (user != null) {
       ctx.json(user);
+      ctx.status(HttpCode.OK);
     } else {
       throw new NotFoundResponse("No user with id " + id + " was found.");
     }
