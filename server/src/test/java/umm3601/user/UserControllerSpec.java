c1102678c2918820477a458d60b360d588933f5e
Nic McPhee <mcphee@morris.umn.edu>
2022-01-31 17:30:54 -0600

    Fix all the test names in `UserControllerSpec`
    None of the test names in `UserControllerSpec.java` match any Java naming convention, and Checkstyle was *not* happy about them. (In fairness, I'm pretty sure they came from the Javalin testing tutorial, that uses essentially the name naming convention.)
    
    Regardless, this fixes all those and should put us in a happier place on that front.
    
    Closes #172

--- server/src/test/java/umm3601/user/UserControllerSpec.java
+++ server/src/test/java/umm3601/user/UserControllerSpec.java
@@ -51,9 +51,16 @@ public class UserControllerSpec {
     userController = new UserController(db);
   }
 
+  /**
+   * Confirms that we can get all the users.
+   *
+   * @throws IOException
+   */
   @Test
-  public void GET_to_request_all_users() throws IOException {
-    // Call the method on the mock controller
+  public void canGetAllUsers() throws IOException {
+    // Call the method on the mock context, which doesn't
+    // include any filters, so we should get all the users
+    // back.
     userController.getUsers(ctx);
 
     // Confirm that `json` was called with all the users.
@@ -63,11 +70,16 @@ public class UserControllerSpec {
   }
 
   @Test
-  public void GET_to_request_age_25_users() throws IOException {
+  public void canGetUsersWithAge25() throws IOException {
+    // Add a query param map to the context that maps "age"
+    // to "25".
     Map<String, List<String>> queryParams = new HashMap<>();
     queryParams.put("age", Arrays.asList(new String[] {"25"}));
     when(ctx.queryParamMap()).thenReturn(queryParams);
 
+    // Call the method on the mock controller with the added
+    // query param map to limit the result to just users with
+    // age 25.
     userController.getUsers(ctx);
 
     // Confirm that all the users passed to `json` have age 25.
@@ -84,13 +96,13 @@ public class UserControllerSpec {
    * we get a reasonable error code back.
    */
   @Test
-  public void GET_to_request_users_with_illegal_age() {
+  public void respondAppropriatelyToIllegalAge() {
     // We'll set the requested "age" to be a string ("abc")
     // that can't be parsed to a number.
     Map<String, List<String>> queryParams = new HashMap<>();
     queryParams.put("age", Arrays.asList(new String[] {"abc"}));
-
     when(ctx.queryParamMap()).thenReturn(queryParams);
+
     // This should now throw a `BadRequestResponse` exception because
     // our request has an age that can't be parsed to a number.
     Assertions.assertThrows(BadRequestResponse.class, () -> {
@@ -99,12 +111,11 @@ public class UserControllerSpec {
   }
 
   @Test
-  public void GET_to_request_company_OHMNET_users() throws IOException {
-
+  public void canGetUsersWithCompany() throws IOException {
     Map<String, List<String>> queryParams = new HashMap<>();
     queryParams.put("company", Arrays.asList(new String[] {"OHMNET"}));
-
     when(ctx.queryParamMap()).thenReturn(queryParams);
+
     userController.getUsers(ctx);
 
     // Confirm that all the users passed to `json` work for OHMNET.
@@ -116,14 +127,12 @@ public class UserControllerSpec {
   }
 
   @Test
-  public void GET_to_request_company_OHMNET_age_25_users() throws IOException {
-
+  public void canGetUsersWithGivenAgeAndCompany() throws IOException {
     Map<String, List<String>> queryParams = new HashMap<>();
     queryParams.put("company", Arrays.asList(new String[] {"OHMNET"}));
-
     queryParams.put("age", Arrays.asList(new String[] {"25"}));
-
     when(ctx.queryParamMap()).thenReturn(queryParams);
+
     userController.getUsers(ctx);
 
     // Confirm that all the users passed to `json` work for OHMNET
@@ -137,18 +146,20 @@ public class UserControllerSpec {
   }
 
   @Test
-  public void GET_to_request_user_with_existent_id() throws IOException {
+  public void canGetUserWithSpecifiedId() throws IOException {
     String id = "588935f5c668650dc77df581";
     User user = db.getUser(id);
 
     when(ctx.pathParam("id")).thenReturn(id);
+
     userController.getUser(ctx);
+
     verify(ctx).json(user);
     verify(ctx).status(HttpCode.OK);
   }
 
   @Test
-  public void GET_to_request_user_with_nonexistent_id() throws IOException {
+  public void respondAppropriatelyToRequestForNonexistentId() throws IOException {
     when(ctx.pathParam("id")).thenReturn(null);
     Assertions.assertThrows(NotFoundResponse.class, () -> {
       userController.getUser(ctx);
