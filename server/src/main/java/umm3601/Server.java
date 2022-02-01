41e6baa151923039396e3008102aeec2a3e954a6
Paul Friederichsen <floogulinc@gmail.com>
2022-02-01 07:05:45 -0600

    Use newer lambda syntax for users routes in Server.java

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -41,10 +41,10 @@ public class Server {
     // API endpoints
 
     // Get specific user
-    server.get("/api/users/{id}", ctx -> userController.getUser(ctx));
+    server.get("/api/users/{id}", userController::getUser);
 
     // List users, filtered using query parameters
-    server.get("/api/users", ctx -> userController.getUsers(ctx));
+    server.get("/api/users", userController::getUsers);
   }
 
   /***
