e332f843e7998e4949b83aad434fbc2f04438a9d
Nic McPhee <mcphee@morris.umn.edu>
2022-01-31 23:30:39 -0600

    Update server/src/main/java/umm3601/user/User.java
    Co-authored-by: Joe Moonan Walbran <walbr037@morris.umn.edu>

--- server/src/main/java/umm3601/user/User.java
+++ server/src/main/java/umm3601/user/User.java
@@ -19,7 +19,7 @@ package umm3601.user;
 public class User {
   // By default Java field names shouldn't start with underscores.
   // Here, though, we *have* to use the name `_id` to match the
-  // name of the field as used by MongoDB.
+  // name of the field in the database.
   @SuppressWarnings({"MemberName"})
   public String _id;
   public String name;
